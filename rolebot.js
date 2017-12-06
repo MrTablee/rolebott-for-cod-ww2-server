8eaaaf8e63adb9769f23e1eb86f28bf642122a73
Mr.Table <Limegreenxd@gmail.com>
2017-12-06 13:07:35 -0500

    Testing Eris

--- rolebot.js
+++ rolebot.js
@@ -1,153 +1,132 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Eris = require("eris");
+const client = new Eris(TOKEN);
 const fs = require("fs");
 const prefix = "r!"
 
-client.on("guildMemberAdd", (member) => {
-  let guild = member.guild;
-  let guestRole = member.guild.roles.find("name", "Guest");
-  const welcomeChannel = member.guild.channels.find("name", 'reception'); 
-try {  member.guild.channels.find("name", 'reception').send(
+client.on("guildMemberAdd", (guild, member) => {
+  let guestRole = guild.roles.find("Guest");
+  const welcomeChannel = guild.channels.find(c=>c.name==='reception'); 
+try {  guild.channels.find(c => c.name === 'reception').createMessage(
 
-  `Welcome ${member.user} to ${guild.name}, use r!addrole to gain access to your platform's channels!`
-).then(() => (member.addRole(guestRole.id)))
+  `Welcome ${member.username} to ${guild.name}, use r!addrole to gain access to your platform's channels!`
+).then(() => (client.addGuildMemberRole(guild.id, member.id, guestRole.id)))
     } catch (err) {
-      client.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err}\`\`\``);
+      client.channels.get('384821440844922882').createMessage(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err}\`\`\``);
     }
 });
 
-client.on("ready", (member) => {
-  client.user.setPresence(
-    {
-      game:
+client.on("ready", () => {
+  client.editStatus(
       {
         name:
         `r!help | In ${client.guilds.size} servers`,
         type: 0
       }
-    })
+  );
   console.log(`RoleBot is connected to the Discord WebSocket`)
 });
 
+var awaitMessages = {};
+
 client.on("message", message => {
   if (message.author.bot) return;
-  if(message.content.indexOf('r!') !== 0) return;
+  if (!message.channel.guild) return; // If DM
+  
+  if (awaitMessages[message.author.id].channel === message.channel.id) {
+	  let guild = message.channel.guild;
+  let guestRole = (guild.roles.find(r => r.name === "Guest"));    
+  let pcRole = (guild.roles.find(r => r.name === "PC"));    
+  let ps4Role = (guild.roles.find(r => r.name === "PS4"));    
+  let xboxRole = (guild.roles.find(r => r.name === "Xbox"));    
+	  if (message.content === 'PC') { 
+          message.channel.createMessage('Took the PC role from ya');
+		  if (awaitMessages[msg.author.id].role === "take")
+          	client.removeGuildMemberRole(guild.id, message.author.id, pcRole.id)
+		  else
+			client.addGuildMemberRole(guild.id, message.author.id, pcRole.id)
+      }
+        if (message.content === 'Xbox') { 
+          message.channel.createMessage('Took the Xbox role from ya');
+          if (awaitMessages[msg.author.id].role === "take")
+          	client.removeGuildMemberRole(guild.id, message.author.id, xboxRole.id)
+		  else
+			client.addGuildMemberRole(guild.id, message.author.id, xboxRole.id)
+      }
+        if (message.content === 'PS4') { 
+          message.channel.createMessage('Took the PS4 role from ya');
+          if (awaitMessages[msg.author.id].role === "take")
+          	client.removeGuildMemberRole(guild.id, message.author.id, ps4Role.id)
+		  else
+			client.addGuildMemberRole(guild.id, message.author.id, ps4Role.id)
+      }
+	  
+	  delete awaitMessages[message.author.id];
+  } // if we are awaiting for messages in the channel
+  
+  if (!message.content.startsWith(prefix)) return;
 
-  const args = message.content.slice('r!'.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
 const botAdmin = "message.author.id == '233366720062947330' || message.author.id == '209765088196821012' || message.author.id == '277608955855896576' || message.author.id == '246766026052730880'"
-if(message.content === 'r!reboot') {
+if(message.content === prefix+'reboot') {
              if  (message.author.id == '233366720062947330' || message.author.id == '215509157837537280') {
-        message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
+        message.channel.createMessage({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
         description: "Reboot in progress... Please allow RoleBot about 5-10 seconds before use again."}).then(process.exit);
               } else if (message.author.id != '233366720062947330') {
-        message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
+        message.channel.createMessage({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
         description: "This is above your pay grade. (Bot admins only)"})
 }}
-if(message.content === 'r!ping') {
-message.channel.send("Pinging...").then(m => {
-  m.edit(":ping_pong: Time taken: " + (m.createdTimestamp - message.createdTimestamp) + 'ms')});
+if(message.content === prefix+'ping') {
+message.channel.createMessage("Pinging...").then(m => {
+  m.edit(":ping_pong: Time taken: " + (m.timestamp - message.timestamp) + 'ms')});
 }
 
-if(message.content === 'takerole'){
-  let guild = message.guild
-  let guestRole = (guild.roles.find("name", "Guest"));    
-  let pcRole = (guild.roles.find("name", "PC"));    
-  let ps4Role = (guild.roles.find("name", "PS4"));    
-  let xboxRole = (guild.roles.find("name", "Xbox"));    
-  
-  message.channel.sendMessage("What role did ya want taken?")
-  .then(() => {
-    message.channel.awaitMessages(response => response.content === 'PC' || response.content === 'Xbox' || response.content === 'PS4', {
-      max: 1,
-      maxMatches: 5,
-      time: 30000,
-      errors: ['time'],
-    })
-    .then((collected) => {
-        if (collected.first().content === 'PC') { 
-          message.channel.send('Took the PC role from ya');
-          message.guild.member(message.author.id).removeRole(pcRole.id)
-      }
-        if (collected.first().content === 'Xbox') { 
-          message.channel.send('Took the Xbox role from ya');
-          message.guild.member(message.author.id).removeRole(xboxRole.id)
-      }
-        if (collected.first().content === 'PS4') { 
-          message.channel.send('Took the PS4 role from ya');
-          message.guild.member(message.author.id).removeRole(ps4Role.id)
-      }
-  })
-      .catch(() => {
-        message.channel.send('No roles were mentioned...');
-      });
-  });
+if(message.content === prefix+'takerole'){
+  message.channel.createMessage("What role did ya want taken?")
+  .then(m => {awaitMessages[message.author.id] = {channel: message.channel.id, role: "take"};
+  		setTimeout(()=>{
+			message.channel.createMessage('No roles were mentioned...');
+			delete awaitMessages[message.author.id]
+		}, 30000)});
 }
 
-if((message.content.startsWith('r!manual')) || message.content.startsWith('r!m')) {
-  let staffRole = (message.guild.roles.find("name", "Staff"));
-  if(!message.member.roles.has(staffRole.id)) {
-message.channel.send('This command is for Staff only')
+if((message.content.startsWith(prefix+'manual')) || message.content.startsWith(prefix+'m')) {
+  let staffRole = (message.channel.guild.roles.find(r => r.name === "Staff"));
+  if(message.member.roles.indexOf(staffRole.id) === -1) {
+message.channel.createMessage('This command is for Staff only')
 return;
-} else if(message.member.roles.has(staffRole.id)) {
+} else if(message.member.roles.indexOf(staffRole.id) !== -1) {
 if((args[0] === 'takerole') || (args[0] === 't')) {
-  let mentionedMember = message.guild.member(message.mentions.users.first());        let roleToRemove = (message.guild.roles.find('name', args[2]));
-  message.guild.member(mentionedMember.id).removeRole(roleToRemove.id)
+  let mentionedMember = message.channel.guild.members.get(message.mentions[0].id);        let roleToRemove = (message.channel.guild.roles.find(c => c.name === args[2]));
+  client.removeGuildMemberRole(guild.id, message.author.id, roleToRemove.id)
 } else if((args[0] === 'addrole') || (args[0] === 'a')) {
-  let mentionedMember = message.guild.member(message.mentions.users.first());        let roleToRemove = (message.guild.roles.find('name', args[2]));
-  message.guild.member(mentionedMember.id).addRole(roleToRemove.id)
+  let mentionedMember = message.channel.guild.members.get(message.mentions[0].id);        let roleToRemove = (message.channel.guild.roles.find(c => c.name === args[2]));
+  client.addGuildMemberRole(guild.id, message.author.id, roleToRemove.id)
 }}}
 
-if(message.content === 'r!addrole') {
-  let guild = message.guild
-  let guestRole = (guild.roles.find("name", "Guest"));    
-  let pcRole = (guild.roles.find("name", "PC"));    
-  let ps4Role = (guild.roles.find("name", "PS4"));    
-  let xboxRole = (guild.roles.find("name", "Xbox"));    
+if(message.content === prefix+'addrole') {
+
   
-  message.channel.sendMessage("What role did ya want?")
-  .then(() => {
-    message.channel.awaitMessages(response => ['Xbox', 'PC', 'PS4'].includes(response.content), {
-      max: 1,
-      maxMatches: 5,
-      time: 30000,
-      errors: ['time'],
-    })
-    .then((collected) => {
-        if (collected.first().content === 'PC') { 
-          message.channel.send('You now have access to the following PC channels:\n<#377263671895785483>\n<#377263736479678465>\n<#385164138529292298>\nAnd <#385553319457849344>');
-          message.guild.member(message.author.id).addRole(pcRole.id)
-      }
-        if (collected.first().content === 'Xbox') { 
-          message.channel.send('You now have access to the following Xbox channels:\n<#377263472091594752>\n<#377263507173015553>\n<#385164046892007426>\nAnd <#385553280241106955>!');
-          message.guild.member(message.author.id).addRole(xboxRole.id)
-      }
-        if (collected.first().content === 'PS4') { 
-          message.channel.send('You now have access to the following PS4 channels:\n<#377263304915156992>\n<#385164109861355521>\n<#385552955853504524>\nAnd <#377263347189678090>!');
-          message.guild.member(message.author.id).addRole(ps4Role.id)
-      }
-  })
-      .catch(() => {
-        message.channel.send('No roles were mentioned...');
-      });
-  });
+  message.channel.createMessage("What role did ya want?")
+  .then(m => { awaitMessages[message.author.id] = {channel: message.channel.id, role: "get"};
+  		setTimeout(()=>{
+			message.channel.createMessage('No roles were mentioned...');
+			delete awaitMessages[message.author.id]
+		}, 30000)});
 }
 
-if(message.content.startsWith('r!eval')){
+if(message.content.startsWith(prefix+'eval')){
   try {
     if (message.author.id !== '233366720062947330') return;
-  let command = message.content.replace("r!eval ", "");
+  let command = message.content.replace(prefix+"eval ", "");
   let output = eval(command);
-  message.channel.send("\n```\n" + output + "\n```");
+  message.channel.createMessage("\n```\n" + output + "\n```");
   } catch (err) {
-    message.channel.send("`Uh oh... " + err + "` came up... Check your code and try again!")
+    message.channel.createMessage("`Uh oh... " + err + "` came up... Check your code and try again!")
   }}
 });
 
-setInterval(function() {
-    http.get("http://rolebott-for-cod-ww2-server.herokuapp.com");
-    console.warn('TIMEOUT !');
-}, 600000); // every 10 minutes (10*60000)
 
-client.login(process.env.BOT_TOKEN);
+
+client.connect();
