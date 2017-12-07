af3990d20bca6cb5fb6a396b02c909307c12b776
Mr.Table <Limegreenxd@gmail.com>
2017-12-06 21:37:59 -0500

    Typo fix

--- commandssss/ping.js
+++ commandssss/ping.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send("On second, rushing B...").then(m => {
+    message.channel.send("One second, rushing B...").then(m => {
     m.edit(":ping_pong: Objective time: " + (m.createdTimestamp - message.createdTimestamp) + 'ms')});
 }
