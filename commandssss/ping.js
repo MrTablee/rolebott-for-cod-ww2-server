c4e43918943f2acf3ae2e4018c4657eecbe896f8
Mr.Table <Limegreenxd@gmail.com>
2017-12-10 04:44:07 -0500

    Updating ping.js's message

--- commandssss/ping.js
+++ commandssss/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
     message.channel.send("One second, rushing B...").then(m => {
-    m.edit(":ping_pong: Objective time: " + (m.createdTimestamp - message.createdTimestamp) + 'ms')});
+    m.edit(":ping_pong: **Bot ping stats:** \n *Response time:* " + (m.createdTimestamp - message.createdTimestamp) + 'ms \n*Heartbeat ping:* ' + Math.round(client.ping) + "ms \n*Most recent heartbeat pings:* " + client.pings)});
 }
 
