a9b89e36a27a38168777652c6d8ae071d8b44ad4
Mr.Table <Limegreenxd@gmail.com>
2017-12-11 11:20:08 -0500

    Update evaleris.js

--- commandsss/evaleris.js
+++ commandsss/evaleris.js
@@ -1,10 +1,8 @@
 exports.run = (client, message, args) => {
     const Discord = require("eris");
-    client.registerCommand("evaleris", (msg) => {
       let evalCmd = msg.content.replace("..evaleris ", "");
     return eval(evalCmd)},{
       requirements:{ 
         userIDs: ['233366720062947330'] 
       }
-    })
   }
