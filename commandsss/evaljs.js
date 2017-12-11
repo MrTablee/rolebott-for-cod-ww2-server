170e371fd388e40559fc6c93fdeaab2b2e9bcb4d
Mr.Table <Limegreenxd@gmail.com>
2017-12-11 11:05:43 -0500

    Update evaljs.js

--- commandsss/evaljs.js
+++ commandsss/evaljs.js
@@ -1,6 +1,4 @@
 exports.run = (client, message, args) => {
-  const Discord = require("discord.js");
-  const client = new Discord.Client();
   if (message.author.id !== '233366720062947330') return;
 let command = message.content.replace("..evaljs ", "");
 let output = eval(command);
