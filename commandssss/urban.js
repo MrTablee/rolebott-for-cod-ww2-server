2d0b90338ef4211d700260696c36f59920832709
Mr.Table <Limegreenxd@gmail.com>
2017-12-12 12:59:32 -0500

    Update urban.js

--- commandssss/urban.js
+++ commandssss/urban.js
@@ -1,15 +1,15 @@
 const ud = require('urban-dictionary')
 exports.run = (client, message, args) => {
     
-if(message.content === "..urban") {
+if(message.content === "r!urban") {
 ud.random(function (error, entry) {
   if (error) {
     message.channel.send(error.message)
   } else {
     message.channel.sendEmbed({ color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
         description:  ("**Word:**\n" + entry.word + "\n**Definition:**\n" + entry.definition + "\n**Example:**\n" + entry.example)})
-  }})} else if (message.content.startsWith("..urban ")) {    
-   var definition = message.content.replace("..urban ", "")
+  }})} else if (message.content.startsWith("r!urban ")) {    
+   var definition = message.content.replace("r!urban ", "")
     
    ud.term(definition, function (error, entries, tags, sounds) {
      if (error) {
