f0f1df3296db9b840e897b64c846898962a3ad02
Mr.Table <Limegreenxd@gmail.com>
2017-12-12 11:06:57 -0500

    Update addrole.js

--- commandssss/addrole.js
+++ commandssss/addrole.js
@@ -17,17 +17,17 @@ exports.run = (client, message, args) => {
         if (collected.first().content === 'PC') { 
           message.channel.send('You now have access to the following PC channels:\n<#377263671895785483>\n<#377263736479678465>\n<#385164138529292298>\nAnd <#385553319457849344>');
           message.guild.member(message.author.id).addRole(pcRole.id)
-           message.guild.member(message.author.id).addRole(guestRole.id)
+           message.guild.member(message.author.id).removeRole(guestRole.id)
       }
         if (collected.first().content === 'Xbox') { 
           message.channel.send('You now have access to the following Xbox channels:\n<#377263472091594752>\n<#377263507173015553>\n<#385164046892007426>\nAnd <#385553280241106955>!');
           message.guild.member(message.author.id).addRole(xboxRole.id)
-           message.guild.member(message.author.id).addRole(guestRole.id)
+           message.guild.member(message.author.id).removeRole(guestRole.id)
       }
         if (collected.first().content === 'PS4') { 
           message.channel.send('You now have access to the following PS4 channels:\n<#377263304915156992>\n<#385164109861355521>\n<#385552955853504524>\nAnd <#377263347189678090>!');
           message.guild.member(message.author.id).addRole(ps4Role.id)
-           message.guild.member(message.author.id).addRole(guestRole.id)
+           message.guild.member(message.author.id).removeRole(guestRole.id)
       }
   })
       .catch(() => {
