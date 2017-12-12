eb57f43871af61ba4a0d8531c6a911f5b1e96713
Mr.Table <Limegreenxd@gmail.com>
2017-12-12 08:41:00 -0500

    Update SP.js

--- commandssss/SP.js
+++ commandssss/SP.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
-    var changed = message.content.replace("..SP ", "")
+    var changed = args[0]
         if (message.author.id == '233366720062947330') {
             client.user.setGame(changed)
             message.channel.send('Set playing to: `' + changed + '`')
             setTimeout(() => {
-                client.user.setGame("..help | in " + client.guilds.size + " servers!")
+                client.user.setGame("r!help | in " + client.guilds.size + " servers!")
             }, 3600000);
         }
     }
