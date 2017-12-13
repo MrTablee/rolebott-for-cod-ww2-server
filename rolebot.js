d16f5d9fb19f0ce7c00439a4a935bfb012109de1
Mr.Table <Limegreenxd@gmail.com>
2017-12-13 08:32:00 -0500

    Update rolebot.js

--- rolebot.js
+++ rolebot.js
@@ -198,7 +198,14 @@ bot.connect();
 
 
 
-
+rolebotclient.on("messageDelete", (message) => {
+  try{
+  rolebotclient.channels.get(message.guild.channels.find('name', 'logs').id).sendEmbed({ color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
+            description: `**Message Author:** ${message.author.tag}\n**Message Content:** ${message.content}\n**Message Channel:** ${message.channel.name}`});
+    } catch (err) {
+      rolebotclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err}\`\`\``);
+    }
+});
 
 rolebotclient.on("guildMemberAdd", (member) => {
   let guild = member.guild;
