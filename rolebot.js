f8ee7318ae07065736780023dabef50164094072
Mr.Table <Limegreenxd@gmail.com>
2017-12-13 23:56:45 -0500

    Update rolebot.js

--- rolebot.js
+++ rolebot.js
@@ -255,7 +255,8 @@ rolebotclient.on("message", message => {
       });
     });
   }
-  if((message.guild.id === '377259194211893248') && (message.content.includes('youtube.com/')) && (!message.guild.member(message.author.id).roles.exists('name', 'Content Creators')) ){
+  if((message.guild.id === '377259194211893248') && (message.content.includes('youtube.com/')) && (!message.guild.member(message.author.id).roles.exists('name', 'Content Creators'))){
+    try{
   let guestRole = (guild.roles.find("name", "Guest"));    
   let pcRole = (guild.roles.find("name", "PC"));    
   let ps4Role = (guild.roles.find("name", "PS4"));    
@@ -264,7 +265,8 @@ rolebotclient.on("message", message => {
     message.delete()
   message.channel.send(`So uhm... You can't do that... Unless you're a content creator... So I'm gonna go ahead and mute you... ${message.author.tag}`)
   message.guild.member(message.author.id).addRole(muteRole.id)
-  message.author.send(`Hey there, sorry if I muted you wrongfully, but you need the role \`Content Creators\` to send youtube links in ${message.guild.name}`)}
+  message.author.send(`Hey there, sorry if I muted you wrongfully, but you need the role \`Content Creators\` to send youtube links in ${message.guild.name}`)} catch(err)
+  message.channel.send(err)}
   if(message.content.indexOf('r!') !== 0) return;
 
   const args = message.content.slice('r!'.length).trim().split(/ +/g);
