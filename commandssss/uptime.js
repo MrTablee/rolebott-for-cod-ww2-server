243a7bce28b289ca4ad03d646278d0956f7fc2e0
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-17 11:49:12 +0000

    Fixed issue with client.user.avatarURl

--- commandssss/uptime.js
+++ commandssss/uptime.js
@@ -6,7 +6,7 @@ exports.run = (client, msg) => {
     const embed = new Discord.RichEmbed()
         .setTitle('Uptime')
         .setDescription(moment.duration(client.uptime).format(' D [days], H [hours], m [minutes], s [seconds]'))
-        .setThumbnail(client.user.avatarURL())
+        .setThumbnail(client.user.avatarURL)
         .setTimestamp();
         msg.channel.send({
         embed
