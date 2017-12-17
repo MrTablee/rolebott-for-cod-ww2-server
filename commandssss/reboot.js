594bde6f4ab868d2765310bd0c08a669ba17cd3d
Striker <34070194+Strikerrr@users.noreply.github.com>
2017-12-17 12:22:54 +0000

    Update reboot.js

--- commandssss/reboot.js
+++ commandssss/reboot.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-             if  ((message.author.id == '233366720062947330') || (message.author.id == '215509157837537280')) {
+             if  (message.author.id == '233366720062947330' || message.author.id == '215509157837537280') {
         message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
         description: "Reboot in progress... Please allow RoleBot about 5-10 seconds before use again."}).then(process.exit(1));
               } else if (message.author.id != '233366720062947330') {
