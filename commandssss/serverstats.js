a0a609d3b63e0edbe408c5d847357177974777de
Mr.Table <Limegreenxd@gmail.com>
2017-12-18 10:02:46 -0500

    Update serverstats.js

--- commandssss/serverstats.js
+++ commandssss/serverstats.js
@@ -7,7 +7,7 @@ client.guilds.get(message.guild.id).members.forEach(m => {
 if(m.user.bot){botCount++}
 })
       message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
-      description: `Server name: **${message.guild.name}**\nTotal amount of users in the server:**${message.guild.members.size}**\nActual Users: **${(message.guild.members.size)-botCount}**\nBot Users: **${botCount}**\nServer Created on: **${wd}**\nRoles: **${message.guild.roles.size}**\nChannels: **${message.guild.channels.size}**\nOwner: **${message.guild.owner.tag}**\nServer ID: **${message.guild.id}**`});
+      description: `Server name: **${message.guild.name}**\nTotal amount of users in the server:**${message.guild.members.size}**\nActual Users: **${(message.guild.members.size)-botCount}**\nBot Users: **${botCount}**\nServer Created on: **${wd}**\nRoles: **${message.guild.roles.size}**\nChannels: **${message.guild.channels.size}**\nOwner: **${message.guild.owner}**\nOwner ID: **${message.guild.ownerID}**\nServer ID: **${message.guild.id}**`});
               }
     if(args[0] == 'Usercount') {
               message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
