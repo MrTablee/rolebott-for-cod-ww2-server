95a226336583f38e2869d985838709b3c46f96dc
Striker <zzstrikerzzyt@gmail.com>
2017-12-18 21:28:50 +0000

    Fixed a ting

--- commandssss/reboot.js
+++ commandssss/reboot.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
                   description: "Reboot in progress... Please allow RoleBot about 5-10 seconds before use again."
-            }).then(process.exit());
+            }).then(process.exit(1));
       } else if (message.author.id != '233366720062947330') {
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
