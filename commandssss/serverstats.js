b06bb74ac009e5a021fb9a20e055c465279a7671
Mr.Table <Limegreenxd@gmail.com>
2017-12-18 08:33:48 -0500

    Update serverstats.js

--- commandssss/serverstats.js
+++ commandssss/serverstats.js
@@ -2,13 +2,13 @@ exports.run = (client, message, args) => { try{
     var wd = new Date(message.guild.createdTimestamp).toUTCString();
     if(!args[0]){
       message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
-      description: "Guild Name: **"+message.guild.name+"**\nChannel Topic: **"+message.channel.topic+"**\nMessage Type: **"+message.channel.type+"**\nPosition: **"+message.channel.position+"**\nId: **"+message.channel.id+"**\nCreated: **"+wd+"**" });
+      description: `Server name: **${message.guild.name}**\nUsers in the server:**${message.guild.members.size}**\nServer Created on: **${wd}**\nRoles: **${message.guild.roles.size}**\nChannels: **${message.guild.channels.size}**\nOwner: **${message.guild.owner.name}**\nServer ID: **${message.guild.id}**`
               }
     if(args[0] == 'Usercount') {
               message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
       description: `Server Name: ${message.guild.name}\nUser Number: ${message.guild.members.size}` });
               } else {
-              message.channel.send('Insuffiecient Args')
+              message.channel.send('Insufficient Args')
               }
               
               
