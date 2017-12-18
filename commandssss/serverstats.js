fe049e0d848a67256b7b1d19bedcf359e2174ec3
Mr.Table <Limegreenxd@gmail.com>
2017-12-18 12:58:35 -0500

    Update serverstats.js

--- commandssss/serverstats.js
+++ commandssss/serverstats.js
@@ -6,6 +6,7 @@ exports.run = (client, message, args) => { try{
 client.guilds.get(message.guild.id).members.forEach(m => {
 if(m.user.bot){botCount++}
 })
+
       message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
       description: `Server name: **${message.guild.name}**\nTotal amount of users in the server:**${message.guild.members.size}**\nActual Users: **${(message.guild.members.size)-botCount}**\nBot Users: **${botCount}**\nServer Created on: **${wd}**\nRoles: **${message.guild.roles.size}**\nChannels: **${message.guild.channels.size}**\nOwner: **${message.guild.owner}**\nOwner ID: **${message.guild.ownerID}**\nServer ID: **${message.guild.id}**`});
               }
@@ -13,6 +14,13 @@ if(m.user.bot){botCount++}
               message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
       description: `Server Name: ${message.guild.name}\nUser Number: ${message.guild.members.size}` });
               }
+    if(args[0] == 'Users') {
+              message.author.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
+      description: `Server Name: **${message.guild.name}**\nUser Number: **${message.guild.members.size}**\nUsers:`});
+client.guilds.get(message.guild.id).members.forEach(m => {
+message.author.send(m.user.tag)
+})
+    }
               
               
 } catch(err){message.channel.send(err)}}
