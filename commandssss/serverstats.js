faf301c03d7c582e0830186a3480d421da8da6fb
Mr.Table <Limegreenxd@gmail.com>
2017-12-18 01:11:57 -0500

    Update serverstats.js

--- commandssss/serverstats.js
+++ commandssss/serverstats.js
@@ -2,10 +2,11 @@ exports.run = (client, message, args) => { try{
     var wd = new Date(message.guild.createdTimestamp).toUTCString();
     if(!args[0]){
       message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
-      description: "Name: **"+message.channel.name+"**\nTopic: **"+message.channel.topic+"**\nType: **"+message.channel.type+"**\nPosition: **"+message.channel.position+"**\nId: **"+message.channel.id+"**\nCreated: **"+wd+"**" });
-              } else if(args[0] == 'Usercount') {
+      description: "Guild Name: **"+message.guild.name+"**\nChannel Topic: **"+message.channel.topic+"**\nMessage Type: **"+message.channel.type+"**\nPosition: **"+message.channel.position+"**\nId: **"+message.channel.id+"**\nCreated: **"+wd+"**" });
+              }
+    if(args[0] == 'Usercount') {
               message.channel.sendEmbed({ color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
-      description: `Server Name: ${message.guild.name}\nUser Number: ${message.guild.users.size}` });
+      description: `Server Name: ${message.guild.name}\nUser Number: ${message.guild.members.size}` });
               } else {
               message.channel.send('Insuffiecient Args')
               }
