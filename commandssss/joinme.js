aab4fa160b1db79e21e008d01356636bb66ed6a0
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 09:51:25 -0500

    Update joinme.js

--- commandssss/joinme.js
+++ commandssss/joinme.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    client.joinVoiceChannel(message.member.voiceState.channelID).catch((err) => {
+    client.joinVoiceChannel(message.member.voicechannelID).catch((err) => {
         message.channel.send("Error joining voice channel: " + err.stack);
         console.log(err);
     })
