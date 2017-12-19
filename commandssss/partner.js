ff2115af0459cc44b8d952195fab0ab2f200905a
Striker <zzstrikerzzyt@gmail.com>
2017-12-19 18:49:04 +0000

    Added strook description

--- commandssss/partner.js
+++ commandssss/partner.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
-if(!args[0]){
-message.channel.send("How to become a partner:\n\n:one:I, Mr.Table#1352, must be in the server.\n\n:two:I must recieve some kind of way of other's knowing I am partnered with you, whether it be via role, announcement, channel, etc.\n\n:three:Server must have at least 500 users, preferably active.\n\n:four:Server must have more users than bots by a ratio of at least 10:1(1 bot per 10 users at the most).\n\n:five:Must have at least 1 of my bots in the server.")
-}
-if (args[0] === '1'){
-message.channel.send('**Call Of Duty Community Server**')
-}
+    if (!args[0]) {
+        message.channel.send('How to become a partner:\n\n:one:I, Mr.Table#1352, must be in the server.\n\n:two:I must recieve some kind of way of other\'s knowing I am partnered with you, whether it be via role, announcement, channel, etc.\n\n:three:Server must have at least 500 users, preferably active.\n\n:four:Server must have more users than bots by a ratio of at least 10:1(1 bot per 10 users at the most).\n\n:five:Must have at least 1 of my bots in the server.')
+    }
 
+    if (args[0] === '1') {
+        message.channel.send('**Call Of Duty Community Server**');
+    }
 
-}
+    if (args[0] === '2') {
+        message.channel.send('**Striker\'s Coding Cave**\n\nStriker made this server to be a humble home to all of his Discord Bots.\nThe server is very active, and personally I think it is awesome.');
+    }
+};
