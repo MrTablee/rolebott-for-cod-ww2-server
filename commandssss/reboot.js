a1de28f44fcfeee8f66bf8f0cfa8d21bdb585600
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 08:24:21 -0500

    Update reboot.js

--- commandssss/reboot.js
+++ commandssss/reboot.js
@@ -1,9 +1,13 @@
 exports.run = (client, message, args) => {
       if (message.author.id == '233366720062947330') {
+            try{
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
                   description: "Reboot in progress... Please allow RoleBot about 5-10 seconds before use again."
             }).then(process.exit());
+                     } catch (err) {
+            rolebotclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
+        }
       } else if (message.author.id != '233366720062947330') {
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
