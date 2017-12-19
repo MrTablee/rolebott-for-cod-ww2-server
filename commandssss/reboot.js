9bb68982e4ab58e993fb7f181cb80128221671a8
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 13:28:04 -0500

    Update reboot.js

--- commandssss/reboot.js
+++ commandssss/reboot.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
-      if (message.author.id === '233366720062947330' || message.author.id === '215509157837537280') {
+      if (message.author.id === '233366720062947330') {
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
                   description: "Reboot in progress... Please allow RoleBot about 5-10 seconds before use again."
-            }).then(client.user.setGame('Back in a sec 👋')).then(process.exit());
+            }).then(process.exit());
       } else if (message.author.id !== '233366720062947330') {
             message.channel.sendEmbed({
                   color: (Math.floor(Math.random() * (10000000 - 1 + 1))) + 1,
