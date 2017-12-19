22deed0b23cff6bf81cf19e7b314149e4d422a3c
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 01:29:36 -0500

    Create guildMemberAdd.js

---
+++ rolebotevents/guildMemberAdd.js
@@ -0,0 +1,10 @@
+exports.run = (member) => {
+let guild = member.guild;
+  let guestRole = member.guild.roles.find('name', 'Guest');
+  const welcomeChannel = member.guild.channels.find('name', 'reception');
+  try {
+    member.guild.channels.find('name', 'reception').send(`Welcome ${member.user} to ${guild.name}, use r!addrole to gain access to your platform's channels!`).then(() => (member.addRole(guestRole.id)))
+  } catch (err) {
+    rolebotclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
+  }
+}
