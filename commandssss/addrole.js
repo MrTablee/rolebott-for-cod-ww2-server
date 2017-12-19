b08ff4b377d0feb72d5f5cb88c6fa8fa59a94677
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 16:25:27 -0500

    Making shit universal

--- commandssss/addrole.js
+++ commandssss/addrole.js
@@ -4,7 +4,8 @@ exports.run = (client, message) => {
   const pcRole = (guild.roles.find('name', 'PC'));
   const ps4Role = (guild.roles.find('name', 'PS4'));
   const xboxRole = (guild.roles.find('name', 'Xbox'));
-
+  const saidRole = (guild.roles.find('name', (message.content.replace("r!addrole ", ""))));
+if(message.guild.id === '377259194211893248') {
   message.channel.sendMessage('What role did ya want?  (This is case sensitive, so const me know `Xbox, PS4, or PC` And I\'ll give you that role)')
     .then(() => {
       message.channel.awaitMessages(response => ['Xbox', 'PC', 'PS4'].includes(response.content), {
@@ -34,4 +35,13 @@ exports.run = (client, message) => {
           message.channel.send('No roles were mentioned...');
         });
     });
+} else if (message.guild.id !== '377259194211893248') {
+if (!args[0]) {message.channel.send('Please mention what role you wanted')
+              }
+  if (!saidRole){message.channel.send(`The role: **${saidRole}** doesn't seem to be around. Check to make sure you used the right capitals!`)
+                } else if (saidRole) {
+                message.channel.send(`Giving *${message.author.tag}* the role **${saidRole}**!`)
+                }
+
+}
 };
