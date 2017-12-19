29a2ecf80efb128bc533f54976b5b04b42ecd149
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 08:16:46 -0500

    This probably isn't right

--- rolebotevents/messageDelete.js
+++ rolebotevents/messageDelete.js
@@ -2,7 +2,7 @@ exports.run = (rolebotclient, message) => {
         try {
             rolebotclient.channels.get(message.guild.channels.find('name', 'logs').id).sendEmbed({
                 color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
-                description: `**Message Author:** ${message.author.tag}\n**Message Content:** ${message.content}\n**Message Channel:** ${message.channel.name}`});
+                description: `**Message Author:** ${message.author.tag}\n**Message Content:** ${message.content}\n**Message Channel:** ${message.channel.name}`}
         } catch (err) {
             rolebotclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
         }
