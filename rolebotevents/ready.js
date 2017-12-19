5ca5497ddc7127e1e2361359eee311bd7dd183db
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 00:38:17 -0500

    Update ready.js

--- rolebotevents/ready.js
+++ rolebotevents/ready.js
@@ -2,7 +2,7 @@ exports.run = (client) => {
 client.on('ready', (member) => {
   client.user.setPresence({
     game: {
-      name: `r!help | In ${rolebotclient.guilds.size} servers`,
+      name: `r!help | In ${rolebotclient.guilds.size} servers!`,
       type: 0
     }
   })
