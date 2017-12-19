f7e4d6d207b1519fbc8d33fa7fbd92629e8e7096
Mr.Table <Limegreenxd@gmail.com>
2017-12-19 16:53:47 -0500

    Update partner.js

--- commandssss/partner.js
+++ commandssss/partner.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
         message.channel.send('How to become a partner:\n\n:one:I, Mr.Table#1352, must be in the server.\n\n:two:I must recieve some kind of way of other\'s knowing I am partnered with you, whether it be via role, announcement, channel, etc.\n\n:three:Server must have at least 500 users, preferably active.\n\n:four:Server must have more users than bots by a ratio of at least 10:1(1 bot per 10 users at the most).\n\n:five:Must have at least 1 of my bots in the server.')
     }
     if (args[0] === '1') {
-        message.channel.send('**Call Of Duty Community Server**');
+        message.channel.send(`**Call Of Duty Community Server**\n\nGreetings, ${message.author.username}!\nDo you feel like you play better when you have a team? Tired of logging on to see your friends are offline or they are busy playing something else? Don’t want to jump in a match, and dominate the whole time, just to lose? Who does?! Join our Discord Server! With a constantly growing population, we are looking to make the task of finding competent and elite players as simple as typing “@“. With our platform specific categories, and game specific channels, you won’t have to worry about being asked to play a game you don’t have, on a platform you don’t own. Our library of games is ever evolving and not just limited to a single game. We have big plans for the server, so give us a shot. Help us out with gaining popularity and we can help you out with finding teammates for your online gaming experience!`);
     }
     if (args[0] === '2') {
         message.channel.send('**Striker\'s Coding Cave**\n\nStriker made this server to be a humble home to all of his Discord Bots.\nThe server is very active, and personally I think it is awesome.');
