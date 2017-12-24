7239fb2a3024bfaa7b5da0b7497f2586aff8b7b5
MrTablee <limegreenxd@gmail.com>
2017-12-24 01:59:36 -0500

    Probs not gonna work but whatevs

--- commandssss/test.js
+++ commandssss/test.js
@@ -3,7 +3,6 @@ exports.run = (client, message, args) => {
         database.query('SELECT points FROM users WHERE userId = $1', [message.mentions.first().id], (err, res) => {
             if (err) {console.log(err); return}
             let points = res.rows[0];
-            console.log('Before checking: '+points);
             if (!points) {points = {
                 points: 0,
                 level: 0
@@ -11,7 +10,6 @@ exports.run = (client, message, args) => {
             database.query('INSERT INTO users (points, userId) VALUES ($1, $2)', [JSON.stringify(points), message.author.id]);
             }
             else points = JSON.parse(res.rows[0].points);
-            console.log('After checking: '+points);
            
             for(i=0;i<100;i++){
             points.points++;
