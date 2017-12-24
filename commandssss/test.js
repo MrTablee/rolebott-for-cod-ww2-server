30c2ce693c2f13b67e273af6c9189ff5c81685c1
MrTablee <limegreenxd@gmail.com>
2017-12-24 03:54:24 -0500

    Testng I guess

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,13 +1,3 @@
-exports.run = (client, message, args) => {
-  const pg = require('pg')
-  const database = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  });
-  database.connect();
-            database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
-              if(err) {message.channel.send(err)}
-              if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
-              message.channel.send(`Your current level is ${res.rows[0].level}`);
-            });
+if(message.conent.startsWith('r!test')) {
+              message.channel.send(`Ass lube`);
 };
--- rolebot.js
+++ rolebot.js
@@ -253,6 +253,13 @@ rolebotclient.on('message', message => {
             points.level = curLevel;
             message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
         }
+        if(message.conent.startsWith('r!test')) {
+          database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
+            if(err) {message.channel.send(err)}
+            if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
+            message.channel.send(`Your current level is ${res.rows[0].level}`);
+          });
+};
 	   
 		
         if (message.content.startsWith(prefix + "level")) {
