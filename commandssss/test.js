54f73224bc498c04568f5b04daad54471d89c003
MrTablee <limegreenxd@gmail.com>
2017-12-24 02:30:24 -0500

    This prob won't work

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,22 +1,10 @@
 exports.run = (client, message, args) => {
     try {
-        database.query('SELECT points FROM users WHERE userId = $1', [message.author.id], (err, res) => {
-            if (err) {console.log(err); return}
-            let points = res.rows[0];
-            if (!points) {points = {
-                points: 0,
-                level: 0
-            };
-            database.query('INSERT INTO users (points, userId) VALUES ($1, $2)', [JSON.stringify(points), message.author.id]);
-            }
-            else points = JSON.parse(res.rows[0].points);
-            points.points++
-            points.points++
-            points.points++
-            points.points++
-            points.points++
-            
-        
-});
+        if (message.content.startsWith('r!level')) {
+            database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
+              if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
+              message.channel.send(`Your current level is ${res.rows[0].level}`);
+            });
+          }
     } catch(err){message.channel.send(err)}
 };
