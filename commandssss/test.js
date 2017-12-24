b3d7c258bcf6a593ecc48e0613af44526365de08
MrTablee <limegreenxd@gmail.com>
2017-12-24 02:50:39 -0500

    Attempt to fix I guess

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,8 +1,6 @@
 exports.run = (client, message, args) => {
-    try {
             database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
               if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
               message.channel.send(`Your current level is ${res.rows[0].level}`);
             });
-    } catch(err){message.channel.send(err)}
 };
