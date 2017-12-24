f77881d4814cbb9ffa6d7c821fcbb488a6ac7a5d
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2017-12-24 09:00:51 +0100

    Update test.js

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,11 +1,5 @@
 exports.run = (client, message, args) => {
-    const pg = require('pg');
-    const database = new pg.Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    });
-    database.connect();
-            database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
+            args[0].query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
               if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
               message.channel.send(`Your current level is ${res.rows[0].level}`);
             });
