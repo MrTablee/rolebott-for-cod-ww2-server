5252b979e994dcdf59f803a8a9bec4a89fbd62b6
MrTablee <limegreenxd@gmail.com>
2017-12-24 02:53:52 -0500

    Defining database

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,4 +1,9 @@
 exports.run = (client, message, args) => {
+    const database = new pg.Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    });
+    database.connect();
             database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
               if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
               message.channel.send(`Your current level is ${res.rows[0].level}`);
