040e396069e12621298d10567e7ee6c412f58eba
Mr.Table <Limegreenxd@gmail.com>
2017-12-24 01:10:16 -0500

    Testing purpose :o

--- rolebot.js
+++ rolebot.js
@@ -253,12 +253,27 @@ rolebotclient.on('message', message => {
             points.level = curLevel;
             message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
         }
+	    
         if (message.content.startsWith(prefix + "test")) {
-            for (i = 0; i < 666; i++) {
-                points.points++;
-                points.level++;
-            }
+    database.query('SELECT points FROM users WHERE userId = $1', [message.mentions.first().id], (err, res) => {
+        if (err) {console.log(err); return}
+        let points = res.rows[0];
+		console.log('Before checking: '+points);
+        if (!points) {points = {
+            points: 0,
+            level: 0
+        };
+		database.query('INSERT INTO users (points, userId) VALUES ($1, $2)', [JSON.stringify(points), message.author.id]);
+		}
+        else points = JSON.parse(res.rows[0].points);
+		console.log('After checking: '+points);
+	   
+	    for(i=0;i<100;i++){
+        points.points++;
         }
+    
+    }
+		
         if (message.content.startsWith(prefix + "level")) {
             message.reply(`You are currently level ${points.level}, with ${points.points} points.`);
         }
