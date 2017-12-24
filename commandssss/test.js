5a527fdb438d8daf336a6fd45c8cc10dff57b6a0
MrTablee <limegreenxd@gmail.com>
2017-12-24 03:49:04 -0500

    hh

--- commandssss/test.js
+++ commandssss/test.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
   });
   database.connect();
             database.query('SELECT * FROM points WHERE userId = $1', [message.author.id], (err, res) => {
-              if(err) {message.hannel.send(err)}
+              if(err) {message.channel.send(err)}
               if (!res.rows[0]) return message.channel.send('Welp... your current level is 0');
               message.channel.send(`Your current level is ${res.rows[0].level}`);
             });
