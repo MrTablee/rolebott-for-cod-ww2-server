3e33a1d99c2abce9b694bbaf11586d0f2ad50d3b
MrTablee <limegreenxd@gmail.com>
2017-12-25 08:06:37 -0500

    Further possible discord input

--- commandssss/test.js
+++ commandssss/test.js
@@ -1,4 +1,8 @@
 exports.run = (client, message, args) => {
+    if(message.author.id !== '' ) return
+    const mentionedID = args[0]
+    const mentionedAmount = args[1]
+    if(!args) {message.channel.send('You can\'t run this without any args')}
     const pg = require('pg')
     const database = new pg.Client({
         connectionString: process.env.DATABASE_URL,
@@ -6,23 +10,17 @@ exports.run = (client, message, args) => {
     });
     database.connect();
 
-    database.query('SELECT points FROM users WHERE userId = $1', [message.author.id], (err, res) => {
+    database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
         if (err) {console.log(err); return}
         let points = res.rows[0];
-        if(!points)
-        {points = {
-            points: 0,
-            level: 777
-        }
-    database.query('INSERT INTO users (points, userId) VALUES ($1, $2)', [JSON.stringify(points), message.author.id]);
-    }
-        else points = JSON.parse(res.rows[0].points);
+        if(!points){message.channel.send('This user currently has no database stats')}
+        points = JSON.parse(res.rows[0].points);
         console.log('After checking: ' + points);
-        for(i=0;i<args[0];i++){
+        for(i=0;i<mentionedAmount;i++){
         points.points++;
         points.level++;
 }
-    database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), message.author.id], (err, res) => {
+    database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
         if (err) {console.log(err); return}
     });
 });
