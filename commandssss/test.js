e280f6e1a01c95d8dc27c3080f4677a65a1afa31
MrTablee <limegreenxd@gmail.com>
2017-12-25 07:49:42 -0500

    Testssssss

--- commandssss/test.js
+++ commandssss/test.js
@@ -13,7 +13,15 @@ exports.run = (client, message, args) => {
         {points = {
             points: 0,
             level: 777
-        }}
+        }
+    database.query('INSERT INTO users (points, userId) VALUES ($1, $2)', [JSON.stringify(points), message.author.id]);
+    }
+        else points = JSON.parse(res.rows[0].points);
+        console.log('After checking: ' + points);
+        for(i=0;i<100;i++){
+        points.points++;
+        points.level++;
+}
     database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), message.author.id], (err, res) => {
         if (err) {console.log(err); return}
     });
