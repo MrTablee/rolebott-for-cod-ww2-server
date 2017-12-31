5134ee687c1056d82a4b3472bbe495ec1e6c65a0
MrTablee <limegreenxd@gmail.com>
2017-12-31 04:55:17 -0500

    Added stuff to moderate user data

---
+++ commandssss/setcoins.js
@@ -0,0 +1,28 @@
+exports.run = (client, message, args, database, usedPrefix) => {
+    if(message.author.id !== '233366720062947330' ) return
+    const mentionedID = args[0]
+    const mentionedAmount = args[1]
+    if(!args) {message.channel.send('You can\'t run this without any args')}
+    const pg = require('pg')
+    const database = new pg.Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    });
+    database.connect();
+
+    database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
+        if (err) {console.log(err); return}
+        let points = res.rows[0];
+        if(!points){message.channel.send('This user currently has no database stats')}
+        else points = JSON.parse(res.rows[0].points);
+        console.log('After checking: ' + points);
+        let dataa = points.coins
+        points = {
+            coins: mentionedAmount,
+        }
+    database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
+        if (err) {console.log(err); return}
+    });
+});
+message.channel.send(`Set coins to ${mentionedAmount} for: ${mentionedID}!`)
+}
