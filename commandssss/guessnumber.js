93c33b37fd3c5b0916592a073113e165e13a9375
Mr.Table <Limegreenxd@gmail.com>
2018-01-04 14:35:55 -0500

    Hopeful fix

--- commandssss/guessnumber.js
+++ commandssss/guessnumber.js
@@ -39,13 +39,12 @@ if((args[1] < 1) || (args[1] > maxNumberPicked)) {
     message.channel.send(`You must pick a number between 1 and ${maxNumberPicked}!`)
     return;
 }
-if(pickedNumber === chosenNumber){
+if(pickedNumber !== chosenNumber){
+    message.channel.send(`Number was actually ${pickedNumber}, not ${chosenNumber}... you lost ${amountBet} Coins!`)
+} else if(pickedNumber === chosenNumber){
     message.channel.send(`The number was indeed ${pickedNumber}, you won ${amountBet * maxNumberPicked} XP and won back ${Math.floor(amountBet * 0.25)} Coins!`)
     points.coins = points.coins - (0 -(Math.floor(amountBet * maxNumberPicked)))
-    points.xp = points.xp + (maxNumberPicked)
-} else if(pickedNumber !== chosenNumber){
-    message.channel.send(`Number was actually ${pickedNumber}, not ${chosenNumber}... you lost ${amountBet} Coins!`)
-}
+    points.xp = points.xp + (maxNumberPicked) }
             message.channel.send(`${args[0]} ${args[1]} ${args[2]} ${maxNumberPicked} ${pickedNumber} ${chosenNumber}`)
 
     database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
