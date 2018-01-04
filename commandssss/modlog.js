aaa875a411521b2debed7bc1cac3489fda14592c
Mr.Table <Limegreenxd@gmail.com>
2018-01-04 10:49:07 -0500

    Update modlog.js

--- commandssss/modlog.js
+++ commandssss/modlog.js
@@ -1,4 +1,9 @@
 exports.run = (client, message, args, usedPrefix) => {
+        const canKick = message.guild.member(message.author.id).hasPermission('KICK_MEMBERS');
+    
+        if ((!canKick) && message.author.id !== '233366720062947330') {
+            return message.channel.send('You need the permission `KICK_MEMBERS` to use this command.');
+        }
 let person = args[0]
 if(person.length !== 18) {message.channel.send("Proper usage:\n```r!modlog <Target's ID> <Action Used> <Reason>```")}
 let actionUsed = args[1]
