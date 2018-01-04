b712a4d19b07ee1cc1b0777acb49d545c9b2329d
Mr.Table <Limegreenxd@gmail.com>
2018-01-04 10:54:47 -0500

    Updated

--- commandssss/modlog.js
+++ commandssss/modlog.js
@@ -5,9 +5,9 @@ exports.run = (client, message, args, usedPrefix) => {
             return message.channel.send('You need the permission `KICK_MEMBERS` to use this command.');
         }
 let person = args[0]
-if(person.length !== 18) {message.channel.send("Proper usage:\n```r!modlog <Target's ID> <Action Used> <Reason>```")}
 let actionUsed = args[1]
 let reason = message.content.replace(`r!modlog ${args[0]} ${args[1]} `,'')
+if((person.length !== 18) || (!args[0]) || (!args[1]) || (!reason)) {message.channel.send("Proper usage:\n```r!modlog <Target's ID> <Action Used> <Reason>```")}
 client.channels.get(message.guild.channels.find('name', 'modlogs').id).sendEmbed({
         color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
         description: `**Moderator:** ${message.author.tag}\n**Action used:** ${actionUsed}\n**Target:** ${person} (<@${person}>)\n**Reason:** ${reason}`
