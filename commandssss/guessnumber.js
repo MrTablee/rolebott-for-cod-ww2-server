821582463fe33cfb8b2248ff814e6d5ba8465efc
Mr.Table <Limegreenxd@gmail.com>
2018-01-04 14:18:45 -0500

    MHM

--- commandssss/guessnumber.js
+++ commandssss/guessnumber.js
@@ -39,11 +39,11 @@ if((args[1] < 1) || (args[1] > maxNumberPicked)) {
     message.channel.send(`You must pick a number between 1 and ${maxNumberPicked}!`)
     return;
 }
-if(pickedNumber === chosenNumber){
+if(pickedNumber === args[2]){
     message.channel.send(`The number was indeed ${pickedNumber}, you won ${amountBet * maxNumberPicked} XP and won back ${Math.floor(amountBet * 0.25)} Coins!`)
     points.coins = points.coins - (0 -(Math.floor(amountBet * maxNumberPicked)))
     points.xp = points.xp + (maxNumberPicked)
-} else if(pickedNumber !== chosenNumber){
+} else if(pickedNumber !== args[2]){
     message.channel.send(`Number was actually ${pickedNumber}, not ${chosenNumber}... you lost ${amountBet} Coins!`)
 }
 
