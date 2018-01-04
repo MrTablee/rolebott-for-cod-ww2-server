93d446cd05da4097e50131fe997de32d1acfcad6
Mr.Table <Limegreenxd@gmail.com>
2018-01-04 08:44:00 -0500

    Update modlog.js

--- commandssss/modlog.js
+++ commandssss/modlog.js
@@ -1,9 +1,8 @@
 exports.run = (client, message, args, usedPrefix) => {
-const argss = message.content.slice(usedPrefix.length).trim().split(/-+/g);
 let person = args[0]
-if(person.length !== 18) {message.channel.send("Proper usage:\n```r!modlog <Target's ID> -<Reason> <Action Used>```")}
-let reason = argss[0]
-let actionUsed = message.content.replace(`r!modlog ${args[0]} ${argss[0]} `,'')
+if(person.length !== 18) {message.channel.send("Proper usage:\n```r!modlog <Target's ID> <Action Used> <Reason>```")}
+let actionUsed = args[1]
+let reason = message.content.replace(`r!modlog ${args[0]} ${args[1]} `,'')
 client.channels.get(message.guild.channels.find('name', 'modlogs').id).sendEmbed({
         color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
         description: `**Moderator:** ${message.author.tag}\n**Action used:** ${actionUsed}\n**Target:** ${person} (<@${person}>)\n**Reason:** ${reason}`
