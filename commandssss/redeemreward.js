dca45b71ff3da1d48e57fd0afdbbb729f991a4c2
Mr.Table <Limegreenxd@gmail.com>
2018-01-08 10:03:53 -0500

    Add redeeming rewards

---
+++ commandssss/redeemreward.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, args, database, usedPrefix) => {
+    
+        const mentionedID = message.author.id
+        let rewardCoins = (Math.floor(Math.random() * ((10 * points.rewardLevel) - 1 + 1))) + 1;
+        let rewardXP = (Math.floor(Math.random() * ((10 * points.rewardLevel) - 1 + 1))) + 1;
+        
+        database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
+            if (err) {console.log(err); return}
+            let points = res.rows[0];
+            if(!points){message.channel.send('This user currently has no database stats')}
+            else points = JSON.parse(res.rows[0].points);
+            if(rewardsAvailable > 0){
+            points.rewardsAvailable--
+            points.xp = points.xp + rewardXP
+            points.coins = points.coins + rewardCoins
+            message.channel.send(`*${message.author.username} collected their reward, which was worth ${rewardXP} XP, and ${rewardCoins} Coins!*`)            
+            }
+        database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
+            if (err) {console.log(err); return}
+        });
+    });
+    }
