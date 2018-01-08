40ec799d7e9cee7544e8f1fba94135fd3a0d290e
MrTablee <limegreenxd@gmail.com>
2018-01-07 22:19:48 -0500

    .

--- commandssss/daily.js
+++ commandssss/daily.js
@@ -11,9 +11,7 @@ exports.run = (client, message, args, database, usedPrefix) => {
             let rewardCoins = (Math.floor(Math.random() * (((points.level) * 10) - 1 + 1))) + 1;
             let rewardXP = (Math.floor(Math.random() * (((points.level) * 10) - 1 + 1))) + 1;    
     
-            var countDownDate = points.dailyCooldown
-            var noww = new Date().getTime();
-            
+            var countDownDate = points.dailyCooldown            
                 var x = setInterval(function() {
                     
                       var now = new Date().getTime();
@@ -28,12 +26,20 @@ exports.run = (client, message, args, database, usedPrefix) => {
                     }, 1000);
     
             console.log('After checking: ' + points);
-            if(points.dailyCooldown < noww){
+            if(points.dailyCooldown < now){
             points.xp = points.xp + rewardXP
             points.coins = points.coins + rewardCoins
             points.dailyCooldown = new Date().getTime() + 86400000            
             message.channel.send(`*${message.author.username} collected their daily reward of ${rewardCoins} Coins and ${rewardXP} XP!*`)            
             } else {
+                var now = new Date().getTime();
+                
+                  var distance = countDownDate - now;
+                
+                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
                 message.channel.send(`Gotta wait for another ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds before retrieving another daily!`)
             }
         database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
