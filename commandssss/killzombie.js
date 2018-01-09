93471c767cb669be2a08af636ecb866a181bfab9
Mr.Table <Limegreenxd@gmail.com>
2018-01-09 10:19:30 -0500

    Update killzombie.js

--- commandssss/killzombie.js
+++ commandssss/killzombie.js
@@ -1,15 +1,15 @@
 exports.run = (client, message, args, database, usedPrefix) => {
     
         const mentionedID = message.author.id
-        let rewardCoins = (Math.floor(Math.random() * (10 - 1 + 1))) + 1;
-        let rewardXP = (Math.floor(Math.random() * (10 - 1 + 1))) + 1;
+
         
         database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
             if (err) {console.log(err); return}
             let points = res.rows[0];
             if(!points){message.channel.send('This user currently has no database stats')}
             else points = JSON.parse(res.rows[0].points);
-    
+        let rewardCoins = (points.coinBoostLevel * (Math.floor(Math.random() * (10 - 1 + 1))) + 1);
+        let rewardXP = (points.xpBoostLevel * (Math.floor(Math.random() * (10 - 1 + 1))) + 1);
             var countDownDate = points.zombieCooldown
                 
                   var now = new Date().getTime();
