ca44cc440b24c781ad1132de8b5609239271258c
Mr.Table <Limegreenxd@gmail.com>
2018-01-09 09:45:05 -0500

    Create stats.js

---
+++ commandssss/stats.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args, database) => {
+    database.query('SELECT points FROM users WHERE userId = $1', [message.author.id], (err, res) => {
+        if (err) {console.log(err); return}
+        let points = res.rows[0];
+        if(!points){message.channel.send('This user currently has no database stats')}
+        else points = JSON.parse(res.rows[0].points);
+        let usrPoints = points.points
+        let usrLevel = points.level
+        let usrAwards = points.awards
+        let usrKills = points.zombiesSlain
+        let usrCoins = points.coins
+        let usrXP = points.xp
+        let coinsNeeded = (((usrLevel + 1) * 10)**2)
+message.channel.send(`Your stats are:\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP\nReward Level: ${points.rewardLevel}\nRewards Available: ${points.rewardsAvailable}\nDaily cooldown: ${Math.floor((points.dailyCooldown - (new Date().getTime()))/3600000)} Hours`)
+    });
+}
