e2fc352b2a06d7ed2cc562b2ee573bf8566da65e
Mr.Table <Limegreenxd@gmail.com>
2018-01-10 12:23:39 -0500

    Update profile.js

--- commandssss/profile.js
+++ commandssss/profile.js
@@ -9,19 +9,8 @@ exports.run = (client, message, args, database, Discord) => {
         let points = res.rows[0];
         if(!points){message.channel.send('This user currently has no database stats')}
         else points = JSON.parse(res.rows[0].points);
-        let usrPoints = points.points
-        let usrLevel = points.level
-        let usrAwards = points.awards
-        let usrKills = points.zombiesSlain
-        let usrCoins = points.coins
-        let usrXP = points.xp
-        let coinsNeeded = (((usrLevel + 1) * 10)**2)
-        if((points.level > 0) && (points.level < 6)){
-message.channel.send(`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP`)
-        } else if((points.level > 5) && (points.level < 16)){
-message.channel.send(`\`\`\`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP\`\`\``)
-        } else if(points.level > 15){
-const profileEmbed = new Discord.RichEmbed()
+                let coinsNeeded = (((points.level + 1) * 10)**2)
+        const profileEmbed = new Discord.RichEmbed()
 .setTitle(`<@${mentionedID}>'s profile`)
 .addField(`VIP Level: ${points.vipLevel}`)
 .addField(`Current Level: ${points.level}`)
@@ -34,6 +23,17 @@ const profileEmbed = new Discord.RichEmbed()
 .setTimestamp()
 .setAuthor(message.author.username)
 .setFooter(`${client.user.username}#${client.user.discriminator}`, client.user.displayAvatarURL)
+        let usrPoints = points.points
+        let usrLevel = points.level
+        let usrAwards = points.awards
+        let usrKills = points.zombiesSlain
+        let usrCoins = points.coins
+        let usrXP = points.xp
+        if((points.level > 0) && (points.level < 6)){
+message.channel.send(`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP`)
+        } else if((points.level > 5) && (points.level < 16)){
+message.channel.send(`\`\`\`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP\`\`\``)
+        } else if(points.level > 15){
 message.channel.send(profileEmbed)
         }
     
