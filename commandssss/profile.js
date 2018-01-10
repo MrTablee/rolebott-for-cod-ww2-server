3a34bc53ed2ffb4b3aacb6af691a99a849c48bc7
Mr.Table <Limegreenxd@gmail.com>
2018-01-10 13:19:29 -0500

    Update profile.js

--- commandssss/profile.js
+++ commandssss/profile.js
@@ -29,11 +29,11 @@ exports.run = (client, message, args, database) => {
         let usrKills = points.zombiesSlain
         let usrCoins = points.coins
         let usrXP = points.xp
-        if(points.vipLevel = 1){
+        if(points.vipLevel === "1"){
 message.channel.send(`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP`)
-        } else if(points.vipLevel = 2){
+        } else if(points.vipLevel === "2"){
 message.channel.send(`\`\`\`<@${mentionedID}>'s profile:\nVIP Level: ${points.vipLevel}\nCurrent Level: ${usrLevel}\nCurrent Points: ${usrPoints}\nAwards:${usrAwards}\nZombies slain: ${usrKills}\nXP: ${usrXP}\nCoins: ${usrCoins}\nXP needed to level up: ${coinsNeeded} XP\`\`\``)
-        } else if(points.vipLevel = 3){
+        } else if(points.vipLevel === "3"){
 message.channel.send(profileEmbed)
         }
     
