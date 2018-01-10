1bcf7738e1b599885326d76fa2b00ae2e59e6976
Mr.Table <Limegreenxd@gmail.com>
2018-01-10 13:22:05 -0500

    Update buy.js

--- commandssss/buy.js
+++ commandssss/buy.js
@@ -52,9 +52,9 @@ if (amountWanted % 1 !== 0){
             message.channel.send(`*${message.author.username}, you don't have enough coins or you're too high of a VIP level*`)
             return;
             }
-            if ((args[0] === '6') && (points.coins > 50000) && (points.vipLevel = 1)){
+            if ((args[0] === '6') && (points.coins > 50000) && (points.vipLevel = "1")){
             points.coins = points.coins - (50000)
-            points.vipLevel++
+            points.vipLevel = "2"
             message.channel.send(`*${message.author.username} upgraded their VIP level to Level ${points.vipLevel}!*`)  
             }
             if ((args[0] === '5') && (points.coins > (500 * amountWanted))){
