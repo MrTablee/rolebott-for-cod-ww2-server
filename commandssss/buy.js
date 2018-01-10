3a0daaa9a010cbf9f794b48e2725834c5228128c
Mr.Table <Limegreenxd@gmail.com>
2018-01-10 12:41:04 -0500

    Update buy.js

--- commandssss/buy.js
+++ commandssss/buy.js
@@ -48,13 +48,13 @@ if (amountWanted % 1 !== 0){
             message.channel.send(`*${message.author.username}, you don't have enough coins*`)
             return;
             }
-            if ((args[0] === '6') && ((points.coins < 50000))){
-            message.channel.send(`*${message.author.username}, you don't have enough coins*`)
+            if ((args[0] === '6') && (points.coins < 50000) && (points.vipLevel != 1)){
+            message.channel.send(`*${message.author.username}, you don't have enough coins or you're too high of a VIP level*`)
             return;
             }
             if ((args[0] === '6') && (points.coins > 50000) && (points.vipLevel = 1)){
             points.coins = points.coins - (50000)
-            points.vipLevel = 2
+            points.vipLevel++
             message.channel.send(`*${message.author.username} upgraded their VIP level to Level ${points.vipLevel}!*`)  
             }
             if ((args[0] === '5') && (points.coins > (500 * amountWanted))){
