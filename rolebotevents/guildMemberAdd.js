bbba398c56f58a3023f03ad5721f8060a945feee
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 12:12:59 -0500

    Update guildMemberAdd.js

--- rolebotevents/guildMemberAdd.js
+++ rolebotevents/guildMemberAdd.js
@@ -4,7 +4,7 @@ let guild = member.guild;
   let guestRole = guild.roles.find('name', 'Guest');
   const welcomeChannel = member.guild.channels.find('name', 'reception');
   try {
-    member.guild.channels.find('name', 'reception').send(`Welcome ${member.user} to ${guild.name}, use r!addrole to gain access to your platform's channels!`).then(() => (member.addRole(guestRole.id)))
+    member.guild.channels.find('name', 'reception').send(`Welcome ${member.user} to ${guild.name}, use a!addrole to gain access to your platform's channels!`).then(() => (member.addRole(guestRole.id)))
   } catch (err) {
     atlasclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
   }} else if (guild.id !== '377259194211893248'){
