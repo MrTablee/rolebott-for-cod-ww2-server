63eb117c8ded29f8a663ad995ea5810538946a20
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 12:05:06 -0500

    Bug fix

--- rolebotevents/guildMemberDelete.js
+++ rolebotevents/guildMemberDelete.js
@@ -1,13 +1,13 @@
-exports.run = (client, member) => {
+exports.run = (atlasclient, member) => {
 let guild = member.guild;
   const welcomeChannel = member.guild.channels.find('name', 'reception');
   if(!welcomeChannel) return
   try {
     member.guild.channels.find('name', 'reception').send(`There goes ${member.user}... Sorry to see ya go`)
-    rolebotclient.channels.get(message.guild.channels.find('name', 'logs').id).sendEmbed({
+    atlasclient.channels.get(message.guild.channels.find('name', 'logs').id).sendEmbed({
       color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
       description: `**${member.username} left the server!**`
     });
   } catch (err) {
-    rolebotclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
+    atlasclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
   }}
