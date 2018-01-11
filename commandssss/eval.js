c7dc25ac9f24bbfe4848ef25f96dd5940085b6a3
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 09:15:13 -0500

    Update eval.js

--- commandssss/eval.js
+++ commandssss/eval.js
@@ -1,7 +1,7 @@
-exports.run = (client, message, database) => {
+exports.run = (client, message, database, usedPrefix) => {
   try {
     if (message.author.id !== '233366720062947330') return;
-    const command = message.content.replace('r!eval ', '');
+    const command = message.content.replace(`${usedPrefix}eval `, '');
     const output = eval(command);
     message.channel.send('\n```\n' + output + '\n```');
   } catch (err) {
