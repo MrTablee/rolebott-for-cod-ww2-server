76b39e5939d7e129bb236946c7b46a3a0e48c86d
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 12:12:23 -0500

    Update messageDelete.js

--- rolebotevents/messageDelete.js
+++ rolebotevents/messageDelete.js
@@ -2,7 +2,7 @@ exports.run = (atlasclient, message) => {
         try {
             atlasclient.channels.get(message.guild.channels.find('name', 'logs').id).sendEmbed({
                 color: (Math.floor(Math.random() * (16777215 - 1 + 1))) + 1,
-                description: `**Message Author:** ${message.author.tag}\n**Message Content:** ${message.content}\n**Message Channel:** ${message.channel.name}`});
+                description: `__**MESSAGE DELETED**__\n\n**Message Author:** ${message.author.tag}\n**Message Content:** ${message.content}\n**Message Channel:** ${message.channel.name}`});
         } catch (err) {
             atlasclient.channels.get('384821440844922882').send(`ERROR WHEN EXECUTING COMMAND: \`\`\`${err.stack}\`\`\``);
         }
