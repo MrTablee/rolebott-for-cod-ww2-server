e342555f18191c022ec4f14094d6e35cf137147f
Mr.Table <Limegreenxd@gmail.com>
2018-01-12 11:09:24 -0500

    Delete leaderboard.js

--- commandssss/leaderboard.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, message, args, database) => {
-database.query(`SELECT * FROM users ORDER BY userId`).then(rows => {
-                message.channel.send(rows[0] + "\n" + rows[1] + "\n" + rows.0 + "\n" + rows.1);
-        })   
-}
