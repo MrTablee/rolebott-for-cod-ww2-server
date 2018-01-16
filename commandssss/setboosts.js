760a04bcfc918206cf1c75df5e2e8fe2130c8a42
Mr.Table <Limegreenxd@gmail.com>
2018-01-16 12:47:37 -0500

    Create setboosts.js

---
+++ commandssss/setboosts.js
@@ -0,0 +1,23 @@
+exports.run = (client, message, args, database) => {
+    if(message.author.id !== '233366720062947330' ) {
+        message.channel.send(`You don't have proper permissions to run this command!`)
+        return
+    }
+    const mentionedID = args[0]
+    const mentionedAmount = args[1]
+    if(!args) {message.channel.send('You can\'t run this without any args')}
+
+    database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
+        if (err) {console.log(err); return}
+        let points = res.rows[0];
+        if(!points){message.channel.send('This user currently has no database stats')}
+        else points = JSON.parse(res.rows[0].points);
+        console.log('After checking: ' + points);
+points.xpBoostLevel = mentionedAmount
+points.coinBoostLevel = mentionedAmount
+    database.query('UPDATE users SET points = $1 WHERE userId = $2', [JSON.stringify(points), mentionedID], (err, res) => {
+        if (err) {console.log(err); return}
+    });
+});
+message.channel.send(`Set boost levels to level ${mentionedAmount} for: ${mentionedID}!`)
+}
