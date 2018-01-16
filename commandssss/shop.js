f313a55c61ab54d34ef44f4a2206278da07f924a
Mr.Table <Limegreenxd@gmail.com>
2018-01-16 12:36:47 -0500

    Update shop.js

--- commandssss/shop.js
+++ commandssss/shop.js
@@ -11,6 +11,6 @@ exports.run = (client, message, args, database) => {
         let usrCoins = points.coins
         let usrXP = points.xp
         let coinsNeeded = (((usrLevel + 1) * 10)**2)
-message.channel.send(`**Shop Items:**\nID:1 XP boost level up \`Cost: ${points.level * points.xpBoostLevel * 100}\`\nID:2 Coin boost level up \`Cost: ${points.level * points.coinBoostLevel * 500}\`\nID:3 100 XP \`Cost: 500\`\nID:4 1500 XP \`Cost: 5000\`\nID:5 Reward level up \`Cost: 500\`\nID: 6 VIP Level 2 \`Cost: 50000\`\n\n**To buy an item, use r!buy <ID> <AMOUNT>**`)
+message.channel.send(`**Shop Items:**\nID:1 XP boost level up \`Cost: ${points.level * points.xpBoostLevel * 50}\`\nID:2 Coin boost level up \`Cost: ${points.level * points.coinBoostLevel * 100}\`\nID:3 100 XP \`Cost: 500\`\nID:4 1500 XP \`Cost: 5000\`\nID:5 Reward level up \`Cost: 500\`\nID:6 Nothing. Nothing at all \`Cost: Nothing.\`\n\n**To buy an item, use r!buy <ID> <AMOUNT>**`)
     });
 }
