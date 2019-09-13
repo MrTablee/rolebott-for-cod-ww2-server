1b4a34e3baa6fae985a2e1e22d0557ffb2ec1ec4
Mr.Table <Limegreenxd@gmail.com>
2019-09-13 02:00:51 -0400

    Update partners.js

--- commandssss/partners.js
+++ commandssss/partners.js
@@ -1,3 +1,3 @@
 exports.run = (client, message) => {
-message.channel.send('Current list of partners:\nID: 1 **Call of Duty Community**\nID: 2 **Striker\'s Coding Cave**\nID: 3 **Acination**\nID: 4 **Powered Community**\nFeel Free to use r!partner <Partner id> to get more info on a partner or use a!partner for more info on becoming a partner!!');
-};
+message.channel.send('Current list of partners:\nID: 1 **Call of Duty Community**\nID: 2 **Striker\'s Coding Cave**\nID: 3 **Acination**\nFeel Free to use r!partner <Partner id> to get more info on a partner or use r!partner for more info on becoming a partner!!');
+}; 
