05a7e84fe0e06be15c4a0a67b868dc8992b4c085
Mr.Table <Limegreenxd@gmail.com>
2019-09-16 03:40:26 -0400

    Create partners.js

---
+++ commands/partners.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+message.channel.send('Current list of partners:\nID: 1 **Call of Duty Community**\nID: 2 **Striker\'s Coding Cave**\nID: 3 **Acination**\nFeel Free to use A!partner <Partner id> to get more info on a partner!')
+}
