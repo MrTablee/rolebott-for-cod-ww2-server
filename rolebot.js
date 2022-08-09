91d16168dc299a42da4f1d47d1c24131c8f0211c
Mr.Table <Limegreenxd@gmail.com>
2022-08-09 02:10:02 -0400

    Update rolebot.js

--- rolebot.js
+++ rolebot.js
@@ -423,7 +423,7 @@ atlasclient.on('messageUpdate', (oldMsg, newMsg) => {
   }
 });
 
-atlasclient.login(process.env.ATLASTOKEN);
+atlasclient.login(process.env.atlasreal);
 
 alleyclient.on('ready', () => {
 	database.query('CREATE TABLE IF NOT EXISTS scores(userId VARCHAR(18) UNIQUE, points BIGINT DEFAULT 0, level BIGINT DEFAULT 1)', (err, res) => {
